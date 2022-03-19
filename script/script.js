e0ea51c9f4a5e3d2214609ce3583971347f8fae2
NovSergey <92638803+NovSergey@users.noreply.github.com>
2022-03-19 17:31:44 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
         pager: false,
     });
 });
+
+$(document).ready(function(){
+    $(".bxslider_team").bxSlider({
+        controls: false,
+    });
+});
+$(document).ready(function(){
+    $(".flexslider").flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 200,
+        itemMargin: 25,
+        touch: true
+    });
+});
+$(document).ready(function(){
+    $(".accordion").accordion({
+        deffaultOpen: ''
+    });
+});
