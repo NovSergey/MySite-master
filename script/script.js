3568f8090fd5278b5bfcdc950b09615b8ca39cff
NovSergey <92638803+NovSergey@users.noreply.github.com>
2022-04-23 17:35:13 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -41,14 +41,16 @@ $(function(){
     };
     filterList.init();
 });
+
 $(document).ready(function($){
     $('.primary-nav-trigger').on('click', function(){
         $('.menu-icon').toggleClass('is-clicked');
         $('.primary-nav').toggleClass('is-visible');
-        $('body').toggleClass('overflow-hidden');
+        $('.body').toggleClass('overflow-hidden');
     });
 });
-$('.primary-nav').on('click', 'a', function(event){
+
+$('.primary-nav').on("click", "a", function(event){
     event.preventDefault();
     let id = $(this).attr('href'),
         top = $(id).offset().top;
@@ -57,17 +59,67 @@ $('.primary-nav').on('click', 'a', function(event){
 
 let headerHeight = $('.topheader').height();
 $(window).on('scroll', { previousTop: 0 },
-function(){
-    let currentTop = $(window).scrollTop();
-    if(currentTop < this.previousTop){
-        if( !(currentTop > 0) && $('.topheader').hasClass('is-fixed')){
-            $('.topheader').removeClass('is-fixed');
+    function(){
+        let currentTop = $(window).scrollTop();
+        if(currentTop < this.previousTop){
+            console.log("<");
+            if( !(currentTop > 0) && $('.topheader').hasClass('is-fixed')){
+                $('.topheader').removeClass('is-fixed');
+            }
+        }else{
+            console.log(">");
+            if(currentTop > headerHeight && !$('.topheader').hasClass('is-fixed')){
+                $('.topheader').addClass('is-fixed');
+            }
         }
-    }else{
-        if(currentTop > headerHeight && !$('.topheader').hasClass('is-fixed')){
-            $('.topheader').addClass('is-fixed');
-            console.log("true");
-        }
+        this.previousTop = currentTop;  
     }
-    this.previousTop = currentTop;
+);
+
+
+$('#send_form .submit').on("click", function(event){
+    if ($('#name').val() != '' && $('#email').val() != ''){
+        fetch('send.php', {
+            method: 'POST',
+            headers:{
+                'Content-Type': "application/x-www-form-urlencoded"
+            },
+            body: $("#send_form").serialize()
+        }).then((response) => response.json()).then((data) =>{
+            console.log(data)
+            if(data.status === "ok"){
+                $("#send_form").addClass("send_success");
+                setTimeout(() => $("#send_form").removeClass("send_success"), 4000);
+            }
+            if(data.status === "error"){
+                $("#send_form").addClass("send_fail");
+                setTimeout(() => $("#send_form").removeClass("send_fail"), 4000);
+            }
+        });
+    }else{
+        alert("Заполните обязательные поля");
+    }
+});
+
+$(window).load(function(){
+    $(".preload").fadeOut("slow");
+
+function onScrollInit(items){
+items.each(function(){
+    let osElement = $(this),
+        osAnimationClass = osElement.attr('data-os-animation'),
+        osAnimationDelay = osElement.attr('data-os-animation-delay');
+    osElement.css({
+        "animation-delay": osAnimationDelay
+    });
+    osElement.waypoint(function(){
+        osElement.addClass('animated').addClass(osAnimationClass);
+    },
+    {
+        triggerOnce: true,
+        offset: '90%'
+    });
+});
+}
+onScrollInit( $('.os-animation'));
 });
